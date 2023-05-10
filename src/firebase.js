aa3eb349ffe555c64421abdf253498724f748163
sanjaydell <sanjay.anuchuri@gmail.com>
2023-05-10 22:32:05 +0530

    Added firebase authentication

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDdUuiNT3eGDrq2fzrRyP8egFW-OS_y8Z4",
+  authDomain: "blog-post-5b69c.firebaseapp.com",
+  projectId: "blog-post-5b69c",
+  storageBucket: "blog-post-5b69c.appspot.com",
+  messagingSenderId: "504440373088",
+  appId: "1:504440373088:web:252a8dab16325f51b0a805",
+  measurementId: "G-4BXHMXH9XZ"
+}
+
+firebase.initializeApp(config);
+
+export default firebase;
